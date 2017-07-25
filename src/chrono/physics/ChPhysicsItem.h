0b1f402fa290892452898f1438c04c7fd6b1f38d
François Rongère <Francois.Rongere@ec-nantes.fr>
2017-07-25 09:18:03 +0200

    Making the GetSystem method virtual

--- src/chrono/physics/ChPhysicsItem.h
+++ src/chrono/physics/ChPhysicsItem.h
@@ -58,7 +58,7 @@ class ChApi ChPhysicsItem : public ChObj {
     virtual ChPhysicsItem* Clone() const override { return new ChPhysicsItem(*this); }
 
     /// Get the pointer to the parent ChSystem()
-    ChSystem* GetSystem() const { return system; }
+    virtual ChSystem* GetSystem() const { return system; }
 
     /// Set the pointer to the parent ChSystem() and
     /// also add to new collision system / remove from old coll.system
